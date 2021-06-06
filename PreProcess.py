93bd1eb4a8b7822b68fa8d3972087d69781be7e0
jakeoneijk <jakeoneijk@gmail.com>
2021-06-06 02:04:13 +0000

    updata preprocess

--- PreProcess.py
+++ PreProcess.py
@@ -10,14 +10,31 @@ class PreProcess():
         self.util = Util(h_params)
     
     def preprocess_data(self,data_name,output_path):
+        meta_data_list = self.get_train_test_meta_data_list(data_name)
+        
+        for data_type in meta_data_list:
+
+            for i,meta_data in enumerate(meta_data_list[data_type]):
+                print(f"preprocess {meta_data['name']} {data_type} data {i+1}/{len(meta_data_list[data_type])}")
+    
+    def get_train_test_meta_data_list(self,data_name):
+        train_meta_data_list = []
+        test_meta_path_list = []
+
         path_list_path = self.h_params.data.root_path + "/" + data_name + "_path.pkl"
+
         with open(path_list_path,'rb') as file_reader:
             path_list = pickle.load(file_reader)
         
         test_song_list = open(self.h_params.data.root_path + "/" + data_name+"_test_data_list.txt", 'r').read().split('\n')
-        for i,song_data in enumerate(path_list):
-            print(f"preprocess {song_data['name']} {i+1}/{len(path_list)}")
-            data_type =  'test' if song_data['name'] in test_song_list else 'train'
+        
+        for meta_data in enumerate(path_list):
+            if meta_data['name'] in test_song_list:
+                test_meta_path_list.append(meta_data)
+            else:
+                train_meta_data_list.append(meta_data)
+        
+        return {'train': train_meta_data_list, 'test': test_meta_path_list}
     
     def save_dict_feature_by_segment(self,feature_dict,full_size,segment_size,output_path,file_name):
         for start_idx in range(0,full_size,segment_size):
